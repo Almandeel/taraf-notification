8a540ed32b982d2b2d743f3c1261e7b36f3619ec
Almandeel <moezsapry2022@gmail.com>
2020-10-15 15:11:12 +0200

    run npm div

--- public/js/app.js
+++ public/js/app.js
@@ -38207,8 +38207,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/Recruitment-Office/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Recruitment-Office/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /opt/lampp/htdocs/taraf/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /opt/lampp/htdocs/taraf/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
